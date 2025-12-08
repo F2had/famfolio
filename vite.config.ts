3836ed09365fc768b5f9e3866b1c4ddebce018c3
Fahad <42780409+F2had@users.noreply.github.com>
2025-12-08 20:15:54 +0300

    refactor: Replace lucide-vue-next with iconify which includes lucide and simple icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import yaml from '@modyfi/vite-plugin-yaml'
+import Icons from 'unplugin-icons/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     yaml(),
+    Icons({ compiler: 'vue3' }),
   ],
   resolve: {
     alias: {
